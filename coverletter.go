163e0451519bb2c7e5f9e70e89cda4a1a938945c
longfangsong <longfangsong@icloud.com>
2025-11-07 20:58:12 +0100

    escape pos when creating cover letter

--- coverletter.go
+++ coverletter.go
@@ -134,7 +134,13 @@ func RenderAndCompileCoverLetter(templateContent string, data CoverLetterData, o
 	}
 
 	// Generate output filenames
-	pos := strings.ReplaceAll(data.Position, " ", "_")
+	pos := data.Position
+	pos = strings.Map(func(r rune) rune {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') || r == '_' {
+			return r
+		}
+		return '_'
+	}, pos)
 	typstFileName := fmt.Sprintf("Cover_Letter_%s_%s.typ", data.FirstName, pos)
 	pdfFileName := fmt.Sprintf("Cover_Letter_%s_%s.pdf", data.FirstName, pos)
 
